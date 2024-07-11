759851468091cedec12c6c098c0fe7d1ef2f502e
723862870@qq.com <723862870@qq.com>
2024-07-11 10:22:26 +0800

    修改腾讯sms

--- app/common/service/sms/TxSmsService.php
+++ app/common/service/sms/TxSmsService.php
@@ -3,7 +3,6 @@
 namespace app\common\service\sms;
 
 use app\common\service\BaseService;
-use GuzzleHttpExceptionClientException;
 use support\Log;
 use TencentCloud\Common\Credential;
 use TencentCloud\Common\Exception\TencentCloudSDKException;
@@ -54,15 +53,16 @@ class TxSmsService extends BaseService
 
             // 实例化一个 sms 发送短信请求对象,每个接口都会对应一个request对象。
             $req = new SendSmsRequest();
+            // 应用 ID 可前往 [短信控制台](https://console.cloud.tencent.com/smsv2/app-manage) 查看
             $req->SmsSdkAppId = "1400923260";
             /* 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名 */
             // 签名信息可前往 [国内短信](https://console.cloud.tencent.com/smsv2/csms-sign) 或 [国际/港澳台短信](https://console.cloud.tencent.com/smsv2/isms-sign) 的签名管理查看
-            $req->SignName = "坦克对决";
+            $req->SignName = "洛阳蓝景科技有限公司";
             /* 模板 ID: 必须填写已审核通过的模板 ID */
             // 模板 ID 可前往 [国内短信](https://console.cloud.tencent.com/smsv2/csms-template) 或 [国际/港澳台短信](https://console.cloud.tencent.com/smsv2/isms-template) 的正文模板管理查看
             $req->TemplateId = "2208652";
             /* 模板参数: 模板参数的个数需要与 TemplateId 对应模板的变量个数保持一致，若无模板参数，则设置为空*/
-            $req->TemplateParamSet = array("1234");
+            $req->TemplateParamSet = array();
             /* 下发手机号码，采用 E.164 标准，+[国家或地区码][手机号]
              * 示例如：+8613711112222， 其中前面有一个+号 ，86为国家码，13711112222为手机号，最多不要超过200个手机号*/
             $req->PhoneNumberSet = array("+86".$mobile);
