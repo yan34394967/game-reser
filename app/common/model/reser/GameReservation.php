a0260e958d3f7104e9e95928435e2271554767b7
723862870@qq.com <723862870@qq.com>
2024-07-22 10:32:50 +0800

    修改预约记录

--- app/common/model/reser/GameReservation.php
+++ app/common/model/reser/GameReservation.php
@@ -19,31 +19,10 @@ class GameReservation extends Base
      */
     protected $table = 'cc_game_reservation';
 
-    /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'id';
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
-    public $timestamps = false;
-
-    protected $dates = ['create_time', 'update_time'];
-
-    // 设置时间格式
-    protected function serializeDate(DateTimeInterface $date)
-    {
-        return $date->format('Y-m-d');
-    }
-
     // 关联用户表
     public function user()
     {
-        return $this->belongsTo(RustUser::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
 }
